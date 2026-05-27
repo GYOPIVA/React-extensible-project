ffb02a5a87a157e29b21dbab88913ff026149283
Giovany Piveta <boy3giovane@gmail.com>
2026-05-27 00:04:16 -0300

    Update Login.jsx

--- projeto.react/src/components/NavBar/Login.jsx
+++ projeto.react/src/components/NavBar/Login.jsx
@@ -1,4 +1,4 @@
-import './Navbar.css';
+import './NavBar.css';
 import '../Carrinho/Carrinho.css'
 import CardCadastro from '../CardLogin/CardCadastro';
 import CardLogin from '../CardLogin/Cardlogin';
